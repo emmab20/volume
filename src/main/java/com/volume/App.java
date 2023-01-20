607954a08b1427ed903c041fb08f19fbb71ec245
emmab20 <barroga055@gmail.com>
2023-01-19 20:42:30 -0800

    first commit

---
+++ src/main/java/com/volume/App.java
@@ -0,0 +1,11 @@
+package com.volume;
+
+public class App {
+    public static void main(String[] args) {
+        int boxArea = 3;
+        int boxVolume = boxArea * boxArea * boxArea;
+
+        System.out.println("Area: " + boxArea);
+        System.out.println("Volume: " + boxVolume);
+    }
+}
